91b461eb4dd0ec662d5680f87c288a481da44cd3
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-07-02 00:49:13 +0400

    feat: transform raw Lobsters post into schema

--- lobsters-app/data-pipeline/pipeline/transformer.py
+++ lobsters-app/data-pipeline/pipeline/transformer.py
@@ -70,3 +70,16 @@ def transform_post(raw_post_data: dict) -> dict:
     """
     parsed = datetime.fromisoformat(raw_post_data["created_at"])
     created_utc = parsed.timestamp()
+    return{
+      "post_id": raw_post_data["short_id"],
+      "title": raw_post_data["title"],
+      "author": raw_post_data["submitter_user"],
+      "score": raw_post_data["score"],
+      "num_comments": raw_post_data["comment_count"]["username"],
+      "url": raw_post_data["url"],
+      "permalink": raw_post_data["comments_url"],
+      "created_utc": created_utc,
+      "fetched_at": datetime.now(timezone.utc)
+    
+    
+    }
